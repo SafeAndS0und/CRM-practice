c8f5e89456f988c45343c915fe13eed5353b82f8
qqavon <qqavon@gmail.com>
2018-12-28 09:43:59 +0100

    Added update contact

--- backend/api/controllers/contact.js
+++ backend/api/controllers/contact.js
@@ -33,7 +33,7 @@ exports.contact_addContact = (req, res, next) => {
         preparedNewContact
         .save()
         .then(result => {
-            res.status(201).json({
+            return res.status(201).json({
                 msg: 'Dodano nowy kontakt',
                 added: true
             })
@@ -41,7 +41,7 @@ exports.contact_addContact = (req, res, next) => {
     })
     .catch(err => {
         console.log(err)
-        res.status(400).json({
+        return res.status(400).json({
             msg: 'Ups, cos poszlo nie tak',
             added: false
         })
@@ -110,7 +110,7 @@ exports.contact_list = (req, res, next) => {
         if(contacts.length == 0) {
             msg = `Brak kontaktów. Strona: ${page}`
         }
-        res.status(200).json({
+        return res.status(200).json({
             msg: msg,
             contacts: contacts
         })
@@ -133,20 +133,67 @@ exports.contact_detailed = (req, res, next) => {
     .then(contact => {
 
         if(contact) {
-            res.status(200).json({
+            return res.status(200).json({
                 contact: contact
             })
         }
         else {
-            res.status(400).json({
+            return res.status(400).json({
                 msg: 'Kontakt nie istnieje'
             })
         }
     })
     .catch(err => {
         console.log(err)
-        res.status(400).json({
+        return res.status(400).json({
             msg: 'Cos poszło nie tak'
         })
     })
+}
+
+exports.contact_update = (req, res, next) => {
+    const contact_id = req.params.contact_id
+
+    if(!contact_id || !mongoose.Types.ObjectId.isValid(contact_id)) {
+        return res.status(401).json({
+            msg: 'Brak lub niepoprawne id'
+        })
+    }
+    const updateContact = {
+        //basic info
+        firstname: req.body.firstname ? req.body.firstname : '',
+        surname: req.body.surname ? req.body.surname : '',
+        business: req.body.business ? req.body.business : '',
+        modificationTime: new Date(Date.now()).toISOString(),
+        //contact info
+        basicPhone: req.body.basicPhone ? req.body.basicPhone : '',
+        additionalPhone: req.body.additionalPhone ? req.body.additionalPhone : '',
+        basicEmail: req.body.basicEmail ? req.body.basicEmail : '',
+        additionalEmail: req.body.additionalEmail ? req.body.additionalEmail : '',
+        skype: req.body.skype ? req.body.skype : '',
+        canContactViaEmail: req.body.canContactViaEmail ? req.body.canContactViaEmail : false,
+        canContactViaPhone: req.body.canContactViaPhone ? req.body.canContactViaPhone : false,
+        //address info
+        street: req.body.street ? req.body.street : '',
+        buildingNumber: req.body.buildingNumber ? req.body.buildingNumber : '',
+        apartmentNumber: req.body.apartmentNumber ? req.body.apartmentNumber : '',
+        postCode: req.body.postCode ? req.body.postCode : '',
+        city: req.body.city ? req.body.city : '',
+        voivodeship: req.body.voivodeship ? req.body.voivodeship : '',
+        country: req.body.country ? req.body.country : ''
+    }
+
+    Contact.findOneAndUpdate({_id: contact_id}, updateContact, (err, result) => {
+        if(err) {
+            console.log(err)
+            res.status(400).json({
+                msg: 'Error.'
+            })
+        }
+
+        return res.status(200).json({
+            msg: `Zaktualizowano kontakt ${result.number}`
+        })
+    })
+
 }
--- backend/api/routes/contact.js
+++ backend/api/routes/contact.js
@@ -7,5 +7,6 @@ const contactController = require('../controllers/contact')
 Router.post('/addContact', checkAuth, contactController.contact_addContact)
 Router.get('/list/:page/:sort', checkAuth, contactController.contact_list)
 Router.get('/c/:contact_id', checkAuth, contactController.contact_detailed)
+Router.patch('/c/:contact_id', checkAuth, contactController.contact_update)
 
 module.exports = Router
