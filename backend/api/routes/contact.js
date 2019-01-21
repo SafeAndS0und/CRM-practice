6d633d285acfc392f3d2046f070fa646cc1367cc
qqavon <qqavon@gmail.com>
2019-01-21 23:15:20 +0100

    Fixed routes

--- backend/api/routes/contact.js
+++ backend/api/routes/contact.js
@@ -4,7 +4,7 @@ const checkAuth = require('../middlewares/check-auth')
 
 const contactController = require('../controllers/contact')
 
-Router.post('/addContact', checkAuth, contactController.contact_addContact)
+Router.post('/', checkAuth, contactController.contact_addContact)
 Router.get('/list/:page/:sort', checkAuth, contactController.contact_list)
 Router.get('/c/:contact_id', checkAuth, contactController.contact_detailed)
 Router.patch('/c/:contact_id', checkAuth, contactController.contact_update)
